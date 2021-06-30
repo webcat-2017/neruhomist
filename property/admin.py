4d9b1c020ec47bcf7a4ffd2667b3e990e17bd613
LA <webcatcms@gmail.com>
2021-06-30 11:50:33 +0300

    add field phone_3, phone_4, work_schedule, email_1, email_2. Change contact in base.html. Add style class .fa-phone, .fa-envelope-o

--- property/admin.py
+++ property/admin.py
@@ -6,57 +6,26 @@ from django.utils.safestring import mark_safe
 from ckeditor.widgets import CKEditorWidget
 from django.http import Http404, HttpResponseRedirect
 
-
-
-
-
-#admin.site.unregister(Group)
 admin.site.site_header = 'WEBCATcms'
 
 
 class CategoryAdmin(admin.ModelAdmin):
     pass
-    #fieldsets = [
-    #    (None, {'fields':['title', 'image' ,'description', ]}),
-    #    ('SEO налаштування', {'fields':['meta_keywords', 'meta_description']}),
-    #            ]
-    #list_display = ('description')
-    #list_filter = ('title',)
-    #actions_selection_counter = False
-    #actions_on_top = True
-    #actions_on_bottom = True
 
-
-#class ObjectPropertyAdmin(admin.ModelAdmin):
-#    pass
-    #def image_tag(self,obj):
-
-        #return mark_safe('<a href="#" ><img src="{}" width="50" height="50" /></a>'.format(obj.images.url))
-
-
-    #image_tag.short_description = 'Зображення'
-    #list_display = ('title','image_tag','object_type' ,'published', 'status')
-    #date_hierarchy = 'published'
-    #fields = ('title','description')
-    #readonly_fields = ['image_tag']
-
-    #fieldsets = [
-    #    (None, {'fields':['title', 'description', 'object_type', 'locality', 'area', 'street', 'rooms', 'space', 'price', 'image_tag', 'image',]}),
-    #    ('SEO налаштування', {'fields':['meta_keywords', 'meta_description']}),
-    #]
 
 def image_tag(obj):
-
     return mark_safe('<img src="{}" width="50" height="50" />'.format(obj.image.url))
 
+
 class ObjectPropertyImageAdmin(admin.ModelAdmin):
-  pass
+    pass
+
 
 class ObjectPropertyImageInline(admin.StackedInline):
-  model = ObjectPropertyImage
-  max_num = 10
-  extra = 0
-  fields = ['image']
+    model = ObjectPropertyImage
+    max_num = 10
+    extra = 0
+    fields = ['image']
 
 
 class PageAdmin(admin.ModelAdmin):
@@ -67,39 +36,58 @@ class PageAdmin(admin.ModelAdmin):
         ('Розміщення', {'fields': [('menu', 'information', 'about_as')]}),
     ]
 
+
 class ObjectPropertyAdmin(admin.ModelAdmin):
 
-    def image_tag(self,obj):
-
+    def image_tag(self, obj):
         return mark_safe('<img src="{}" width="50" height="50" />'.format(obj.image.url))
 
     image_tag.short_description = 'Зображення'
-    list_display = ('title', 'category_object','image_tag','object_type' ,'published', 'status')
+    list_display = ('title', 'category_object', 'image_tag', 'object_type', 'published', 'status')
 
     inlines = [ObjectPropertyImageInline]
 
-
     fieldsets = [
-        (None, {'fields':['title', 'category_object', 'image' ,'description', 'object_type', 'locality', 'area', 'street', 'rooms', 'total_space', 'living_space', 'kitchen_space', 'height', 'floor', 'storeys', 'price', 'recommended', 'status', ]}),
-        ('SEO налаштування', {'fields':['meta_description']}),
-        ]
+        (None, {
+            'fields': ['title', 'category_object', 'image', 'description', 'object_type', 'locality', 'area', 'street',
+                       'rooms', 'total_space', 'living_space', 'kitchen_space', 'height', 'floor', 'storeys', 'price',
+                       'recommended', 'status', ]}),
+        ('SEO налаштування', {'fields': ['meta_description']}),
+    ]
+
 
 class DontLog:
     def log_addition(self, *args):
         return ''
 
-class SettingAdmin(DontLog,admin.ModelAdmin):
 
+class SettingAdmin(DontLog, admin.ModelAdmin):
     fieldsets = [
-        (None, {'fields':['title', 'base_url', 'phone_1', 'phone_2' ,'link_facebook', 'link_instagram', 'link_telegram', 'site_image',]}),
-        ('SEO налаштування', {'fields':['meta_description']}),
-        ]
+        (None,
+         {'fields': ['title',
+                     'base_url',
+                     'work_schedule',
+                     'phone_1',
+                     'phone_2',
+                     'phone_3',
+                     'phone_4',
+                     'email_1',
+                     'email_2',
+                     'link_facebook',
+                     'link_instagram',
+                     'link_telegram',
+                     'site_image']}),
+        ('SEO налаштування',
+         {'fields': ['meta_description']}),
+    ]
 
     def response_change(self, request, obj):
         return HttpResponseRedirect('/admin')
 
-    def has_add_permission(cls, request):
-        ''' remove add and save and add another button '''
+    def has_add_permission(self, request):
+        """
+            remove add and save and add another button
+        """
         return False
 
     def change_view(self, request, object_id, extra_context=None):
@@ -109,7 +97,6 @@ class SettingAdmin(DontLog,admin.ModelAdmin):
         extra_context['show_delete'] = False
         extra_context['show_history'] = False
         return super(SettingAdmin, self).change_view(request, object_id, extra_context=extra_context)
-
 
 
 admin.site.register(Page, PageAdmin)
--- property/models.py
+++ property/models.py
@@ -8,16 +8,98 @@ from ckeditor_uploader.fields import  RichTextUploadingField
 
 
 class Setting(models.Model):
-    title = models.CharField("Заголовок",max_length=30)
-    base_url = models.URLField("Головне посилання", max_length=50, validators=[validators.URLValidator(schemes=['http','https'])],)
-    meta_description = models.TextField("Мета опис", max_length=100, null=True, blank=True)
-    phone_1 = models.CharField("Телефон №1",max_length=30, validators=[validators.RegexValidator(regex='^[0-9]{10}$')], error_messages={'invalid':'некоректний формат телефону'} )
-    phone_2 = models.CharField("Телефон №2",max_length=30)
-    link_facebook = models.CharField("Посилання Facebook",max_length=50, null=True, blank=True)
-    link_instagram = models.CharField("Посилання Instagram",max_length=50, null=True, blank=True)
-    link_telegram = models.CharField("Посилання Telegram",max_length=50, null=True, blank=True)
-    site_image = FilerImageField(null=True, blank=True, related_name="site_image", on_delete=models.SET_NULL)
+    title = models.CharField(
+        "Заголовок",
+        max_length=30)
 
+    base_url = models.URLField(
+        "Головне посилання",
+        max_length=50,
+        validators=[validators.URLValidator(schemes=['http', 'https'])],)
+
+    meta_description = models.TextField(
+        "Мета опис",
+        max_length=100,
+        null=True,
+        blank=True)
+
+    work_schedule = models.CharField(
+        "Графік роботи",
+        max_length=30,
+        null=True,
+        blank=True)
+
+    phone_1 = models.CharField(
+        "Телефон №1",
+        max_length=30,
+        null=True,
+        blank=True,
+        validators=[validators.RegexValidator(regex='^[0-9]{10}$')],
+        error_messages={'invalid': 'некоректний формат телефону'})
+
+    phone_2 = models.CharField(
+        "Телефон №2",
+        max_length=30,
+        null=True,
+        blank=True,
+        validators=[validators.RegexValidator(regex='^[0-9]{10}$')],
+        error_messages={'invalid': 'некоректний формат телефону'})
+
+    phone_3 = models.CharField(
+        "Телефон №3",
+        max_length=30,
+        null=True,
+        blank=True,
+        validators=[validators.RegexValidator(regex='^[0-9]{10}$')],
+        error_messages={'invalid': 'некоректний формат телефону'})
+
+    phone_4 = models.CharField(
+        "Телефон №4",
+        max_length=30,
+        null=True,
+        blank=True,
+        validators=[validators.RegexValidator(regex='^[0-9]{10}$')],
+        error_messages={'invalid': 'некоректний формат телефону'})
+
+    email_1 = models.CharField(
+        "Почта №1",
+        max_length=30,
+        null=True,
+        blank=True,
+        validators=[validators.validate_email],
+        error_messages={'invalid': 'некоректний формат скриньки'})
+
+    email_2 = models.CharField(
+        "Почта №2",
+        max_length=30,
+        null=True,
+        blank=True,
+        validators=[validators.validate_email],
+        error_messages={'invalid': 'некоректний формат скриньки'})
+
+    link_facebook = models.CharField(
+        "Посилання Facebook",
+        max_length=50,
+        null=True,
+        blank=True)
+
+    link_instagram = models.CharField(
+        "Посилання Instagram",
+        max_length=50,
+        null=True,
+        blank=True)
+
+    link_telegram = models.CharField(
+        "Посилання Telegram",
+        max_length=50,
+        null=True,
+        blank=True)
+
+    site_image = FilerImageField(
+        null=True,
+        blank=True,
+        related_name="site_image",
+        on_delete=models.SET_NULL)
 
     def __str__(self):
         return self.title
@@ -26,33 +108,45 @@ class Setting(models.Model):
         verbose_name = "Налаштування"
         verbose_name_plural = "Налаштування"
 
-
-
     def getSettings(self):
 
-        settings = Setting.objects.get(id = 1)
+        settings = Setting.objects.get(id=1)
 
         def phone_split(x):
-            if x[0:3] in ["039","067", "068", "096","097","098",]:
+            if x[0:3] in ["039", "067", "068", "096", "097", "098"]:
                 logo = "kyivstar.png"
-            elif x[0:3] in ["050", "066", "095", "099",]:
+            elif x[0:3] in ["050", "066", "095", "099"]:
                 logo = "vodafone.png"
-            elif x[0:3] in ["063", "073", "093",]:
+            elif x[0:3] in ["063", "073", "093"]:
                 logo = "lifecell.png"
             else:
                 logo = ''
-            return {'phone':'-'.join([x[0:3], x[3:6], x[6:8], x[8:]]), 'logo':logo,}
+            return {'phone': '-'.join([x[0:3], x[3:6], x[6:8], x[8:]]), 'logo': logo}
 
         settings.phone_1 = phone_split(settings.phone_1)
         settings.phone_2 = phone_split(settings.phone_2)
+        settings.phone_3 = phone_split(settings.phone_3)
+        settings.phone_4 = phone_split(settings.phone_4)
 
         return settings
 
 class Category(models.Model):
-    title = models.CharField("Назва",max_length=30)
+    title = models.CharField(
+        "Назва",
+        max_length=30)
+
     description = RichTextUploadingField("Опис")
-    image = FilerImageField(null=True, blank=True, related_name="image", on_delete=models.SET_NULL)
-    meta_description = models.TextField("Опис", max_length=100, null=True, blank=True)
+    image = FilerImageField(
+        null=True,
+        blank=True,
+        related_name="image",
+        on_delete=models.SET_NULL)
+
+    meta_description = models.TextField(
+        "Опис",
+        max_length=100,
+        null=True,
+        blank=True)
 
     def __str__(self):
         return self.title
@@ -64,30 +158,110 @@ class Category(models.Model):
 
 class ObjectProperty(models.Model):
 
-    category_object = models.ForeignKey(Category, on_delete = models.PROTECT, verbose_name = "Категорія")
-    object_type = models.CharField("Тип Об'єкту", null=False, blank=False, max_length=50,choices = Choices('Квартира', 'Будинок', 'Земельна ділянка', 'Дачний масив', 'Комерційна нерухомість'))
-    title = models.CharField("Назва", max_length=70)
-    image = FilerImageField(verbose_name = "Головне зображення", null=True, blank=True, related_name="image_object_total", on_delete=models.SET_NULL)
+    category_object = models.ForeignKey(
+        Category,
+        on_delete=models.PROTECT,
+        verbose_name="Категорія")
+
+    object_type = models.CharField(
+        "Тип Об'єкту",
+        null=False,
+        blank=False,
+        max_length=50,
+        choices=Choices(
+            'Квартира',
+            'Будинок',
+            'Земельна ділянка',
+            'Дачний масив',
+            'Комерційна нерухомість'))
+
+    title = models.CharField(
+        "Назва",
+        max_length=70)
+
+    image = FilerImageField(
+        verbose_name="Головне зображення",
+        null=True,
+        blank=True,
+        related_name="image_object_total",
+        on_delete=models.SET_NULL)
+
     description = RichTextUploadingField("Опис")
-    meta_description = models.TextField("Мета опис", max_length=160, null=True, blank=True)
-    locality = models.CharField("Населенный пункт", max_length=50)
-    area = models.CharField("Район/Массив", max_length=50, null=True, blank=True)
-    street = models.CharField("Вулиця", max_length=50, null=True, blank=True)
-    rooms = models.IntegerField("Кількість кімнат", choices = Choices(1,2,3,4,5),null=True, blank=True)
-    total_space = models.IntegerField("Загальна площа", null=True, blank=True)
-    living_space = models.IntegerField("Житлова площа", null=True, blank=True)
-    kitchen_space = models.IntegerField("Кухня площа", null=True, blank=True)
-    height = models.FloatField("Висота стелі", null=True, blank=True)
-    floor = models.IntegerField("Поверх", null=True, blank=True)
-    storeys = models.IntegerField("Поверховість", null=True, blank=True)
+
+    meta_description = models.TextField(
+        "Мета опис",
+        max_length=160,
+        null=True,
+        blank=True)
+
+    locality = models.CharField(
+        "Населенный пункт",
+        max_length=50)
+
+    area = models.CharField(
+        "Район/Массив",
+        max_length=50,
+        null=True,
+        blank=True)
+
+    street = models.CharField(
+        "Вулиця",
+        max_length=50,
+        null=True, blank=True)
+
+    rooms = models.IntegerField(
+        "Кількість кімнат",
+        choices=Choices(1, 2, 3, 4, 5),
+        null=True,
+        blank=True)
+
+    total_space = models.IntegerField(
+        "Загальна площа",
+        null=True,
+        blank=True)
+
+    living_space = models.IntegerField(
+        "Житлова площа",
+        null=True,
+        blank=True)
+
+    kitchen_space = models.IntegerField(
+        "Кухня площа",
+        null=True,
+        blank=True)
+
+    height = models.FloatField(
+        "Висота стелі",
+        null=True,
+        blank=True)
+
+    floor = models.IntegerField(
+        "Поверх",
+        null=True,
+        blank=True)
+
+    storeys = models.IntegerField(
+        "Поверховість",
+        null=True,
+        blank=True)
+
     price = models.IntegerField("Ціна")
-    published = models.DateField("Створено", auto_now_add=True, db_index=True)
-    recommended = models.BooleanField("Рекомендований", default=False)
-    status = models.BooleanField("Статус", default=True)
+
+    published = models.DateField(
+        "Створено",
+        auto_now_add=True,
+        db_index=True)
+
+    recommended = models.BooleanField(
+        "Рекомендований",
+        default=False)
+
+    status = models.BooleanField(
+        "Статус",
+        default=True)
 
     def __str__(self):
         return self.title
-
 
     class Meta:
         verbose_name = "Об'єкт"
@@ -95,34 +269,71 @@ class ObjectProperty(models.Model):
         ordering = ['-published']
 
 
-class  ObjectPropertyImage(models.Model):
+class ObjectPropertyImage(models.Model):
 
     def __str__(self):
         return str(self.pk)
-
 
     class Meta:
         verbose_name = "Зображення"
         verbose_name_plural = "Додаткові зображення"
 
-    #image = models.ImageField(upload_to='object_images/%Y/%m/%d/%H/%M/%S/') # здесь укажите куда сохранять изображения
-    image = FilerImageField(null=True, blank=True, related_name="image_object", on_delete=models.SET_NULL)
-    object_property = models.ForeignKey(ObjectProperty, related_name="images",on_delete = models.CASCADE, verbose_name = "Зображення")
+    image = FilerImageField(
+        null=True,
+        blank=True,
+        related_name="image_object",
+        on_delete=models.SET_NULL)
+
+    object_property = models.ForeignKey(
+        ObjectProperty,
+        related_name="images",
+        on_delete=models.CASCADE,
+        verbose_name="Зображення")
+
 
 class Page(models.Model):
-    title = models.CharField("Назва", max_length=70)
-    image = FilerImageField(verbose_name = "Головне зображення", null=True, blank=True, related_name="image_page_total", on_delete=models.SET_NULL)
+    title = models.CharField(
+        "Назва",
+        max_length=70)
+
+    image = FilerImageField(
+        verbose_name="Головне зображення",
+        null=True,
+        blank=True,
+        related_name="image_page_total",
+        on_delete=models.SET_NULL)
+
     description = RichTextUploadingField("Опис")
-    meta_description = models.TextField("Мета опис", max_length=160, null=True, blank=True)
-    published = models.DateField("Створено", auto_now_add=True, db_index=True)
-    menu = models.BooleanField("Меню", default=False)
-    information = models.BooleanField("Підвал (Інформація)", default=False)
-    about_as = models.BooleanField("Підвал (Про нас)", default=False)
-    status = models.BooleanField("Статус", default=True)
+
+    meta_description = models.TextField(
+        "Мета опис",
+        max_length=160,
+        null=True,
+        blank=True)
+
+    published = models.DateField(
+        "Створено",
+        auto_now_add=True,
+        db_index=True)
+
+    menu = models.BooleanField(
+        "Меню",
+        default=False)
+
+    information = models.BooleanField(
+        "Підвал (Інформація)",
+        default=False)
+
+    about_as = models.BooleanField(
+        "Підвал (Про нас)",
+        default=False)
+
+    status = models.BooleanField(
+        "Статус",
+        default=True)
 
     def __str__(self):
         return self.title
-
 
     class Meta:
         verbose_name = "Сторінка"
